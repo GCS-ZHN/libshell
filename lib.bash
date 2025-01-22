224e08afde6462292b029309fe6c39cff02abd81
Zhang.H.N <zhang.h.n@foxmail.com>
2025-01-22 14:23:54 +0800

    feat: specific exit code

--- lib.bash
+++ lib.bash
@@ -1,53 +1,73 @@
 # a shell module for general operations
+LIBSHELL_DEFAULT_OK=0
+LIBSHELL_DEFAULT_ERR=1
+LIBSHELL_ARG_ERR=2
+LIBSHELL_SHELL_NOT_SUPPORTED=3
+LIBSHELL_CMD_NOT_FOUND=4
+LIBSHELL_FILE_EXISTED=5
+LIBSHELL_FILE_TYPE_ERR=6
+LIBSHELL_FILE_IO_ERR=7
 
 SHELL_NAME=$(basename $(ps -p $$ -o comm=))
 
 if [ ${SHELL_NAME} != 'bash' ]; then
     echo "Current shell '${SHELL_NAME}' is not supported!"
-    return 1
+    return $LIBSHELL_SHELL_NOT_SUPPORTED
 fi
 
 function is_source() {
     [ ${BASH_SOURCE[0]} != ${0} ]
     return $?
 }
-export -f is_source
 
 function exit_err() {
-    echo $1 >&2
-    return 1
+    if [ "$#" -lt 1 ]; then
+        exit_err "Usage: exit_err <ERR_MSG> [EXIT_CODE]" ${LIBSHELL_ARG_ERR}
+        return $?
+    fi
+    err_msg=$1
+    exit_code=${2:-${LIBSHELL_DEFAULT_ERR}}
+    echo -e "$err_msg" >&2
+    return $exit_code
 }
 export -f exit_err
 
 function required_args() {
-    if [ "$#" -lt 1 ]; then
-        exit_err "Usage: required_args <ARG_NAME>"
+    if [ "$#" -ne 1 ]; then
+        exit_err "Usage: required_args <ARG_NAME>" ${LIBSHELL_ARG_ERR}
         return $?
     fi
     local arg_name=$1
-    echo ${arg_name}
     if [ -z ${!arg_name} ]; then
-        return 1
+        return ${LIBSHELL_ARG_ERR}
     fi
-    return 0
+    return ${LIBSHELL_DEFAULT_OK}
 }
 export -f required_args
 
 function real_dir() {
+    if [ "$#" -ne 1 ]; then
+        exit_err "Usage: real_dir <DIR_PATH>" ${LIBSHELL_ARG_ERR}
+        return $?
+    fi
     local path=$(realpath -e $1) || return $?
     if [ ! -d "$path" ];then
-        exit_err "'$1' is not a directory"
+        exit_err "'$1' is not a directory" ${LIBSHELL_FILE_TYPE_ERR}
         return $?
     fi
     echo $path
-    return 0
+    return ${LIBSHELL_DEFAULT_OK}
 }
 export -f real_dir
 
 function real_file() {
+    if [ "$#" -ne 1 ]; then
+        exit_err "Usage: real_file <FILE_PATH>" ${LIBSHELL_ARG_ERR}
+        return $?
+    fi
     local path=$(realpath -e $1) || return $?
     if [ ! -f "$path" ];then
-        exit_err "'$1' is not a file"
+        exit_err "'$1' is not a file" ${LIBSHELL_FILE_TYPE_ERR}
         return $?
     fi
     echo $path
@@ -57,15 +77,19 @@ export -f real_file
 
 
 function conda_mv() {
+    if [ "$#" -ne 2 ]; then
+        exit_err "Usage: conda_mv <OLD_CONDA_NAME> <NEW_CONDA_NAME>" ${LIBSHELL_ARG_ERR}
+        return $?
+    fi
     local old_conda_home=$(real_dir $1)
     local new_conda_home=$(realpath $2)
     if [ -e $new_conda_home ]; then
-        exit_err "target path should not be existed!"
+        exit_err "target path should not be existed!" ${LIBSHELL_FILE_EXISTED}
         return $?
     fi
     rsync -av $old_conda_home/ $new_conda_home/
     if [ $? -ne 0 ]; then
-        exit_err "Copy conda home failed!"
+        exit_err "Copy conda home failed!" ${LIBSHELL_FILE_IO_ERR}
         return $?
     fi
     find $new_conda_home -type f \
@@ -83,7 +107,7 @@ export -f conda_mv
 
 function prepend_path() {
     if [ "$#" -lt 2 ]; then
-        exit_err "Usage: prepend_path VAR_NAME PATH [SEPARATOR]"
+        exit_err "Usage: prepend_path <VAR_NAME> <PATH> [SEPARATOR]" ${LIBSHELL_ARG_ERR}
         return $?
     fi
     local var_name=$1
@@ -98,6 +122,13 @@ function prepend_path() {
     else
         export $var_name=$current_value
     fi
-    return 0
+    return ${LIBSHELL_DEFAULT_OK}
 }
 export -f prepend_path
+
+
+if is_source; then
+    exit_err "LibShell is sourced" ${LIBSHELL_DEFAULT_OK}
+else
+    exit_err 'LibShell is library, you should source it by `. lib.bash` or `source lib.bash`'
+fi
